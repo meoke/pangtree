af8b61631825c032679a755ba77c3c61e3679fde
meoke <pedziadkiewicz@gmail.com>
2017-12-01 12:17:05 +0100

    Add tables for visualization example.

--- examples/spotkanie/ebola_100th_maf_block/converted/assets/poagraph.js
+++ examples/spotkanie/ebola_100th_maf_block/converted/assets/poagraph.js
@@ -70,51 +70,51 @@ document.addEventListener("DOMContentLoaded", function() {
                                        '<p>' + 'Sequences per node (mean):' + '\t\t' + poagraph.mean_sources_per_node + '<\p>');
 
     //consensuses info
-//  var divId = document.getElementById("sources_info")
+  var divId = document.getElementById("sources_info")
   for(var i=0; i<consensuses.data.length; i++){
-//    consensus_color = consensuses_colors[consensuses.data[i]['id']%consensuses_colors.length];
-//    consensus_info_div = document.createElement('div')
-//    consensus_info_div.setAttribute("class", "consensus_info")
-//    consensus_info_div.innerHTML += ('<p style=\'color: ' + consensus_color +'\'>' + consensuses.data[i]['name'] + "</p>" +
-//                        '<p>' + 'Nodes count:' + '\t' + consensuses.data[i]['nodes_count'] + '</p>');
+    consensus_color = consensuses_colors[consensuses.data[i]['id']%consensuses_colors.length];
+    consensus_info_div = document.createElement('div')
+    consensus_info_div.setAttribute("class", "consensus_info")
+    consensus_info_div.innerHTML += ('<p style=\'color: ' + consensus_color +'\'>' + consensuses.data[i]['name'] + "</p>" +
+                        '<p>' + 'Nodes count:' + '\t' + consensuses.data[i]['nodes_count'] + '</p>');
 
-//    var table = document.createElement('table');
-//    table.className = "sortable";
-//    var row = table.insertRow(0);
-//    var head_id = row.insertCell(0);
-//    var head_name = row.insertCell(1);
-//    var head_title = row.insertCell(2);
-//    var head_group = row.insertCell(3);
-//    var head_bundle_id = row.insertCell(4);
-//    var head_compatibility = row.insertCell(5);
-//    head_id.innerHTML = "ID"
-//    head_name.innerHTML = "Name"
-//    head_title.innerHTML = "Title"
-//    head_group.innerHTML = "Group"
-//    head_bundle_id.innerHTML = "Bundle ID"
-//    head_compatibility.innerHTML = "Compatibility"
-//
-//    for(var j=0;j<consensuses.data[i]['sources_map'].length;j++){
-//        var row = table.insertRow(j+1);
-//        var id = row.insertCell(0);
-//        var name = row.insertCell(1);
-//        var title = row.insertCell(2);
-//        var group =row.insertCell(3)
-//        var bundle_id = row.insertCell(4);
-//        var compatibility = row.insertCell(5);
-//        id.innerHTML = j
-//        name.innerHTML = sources.data[j]['name']
-//        title.innerHTML = sources.data[j]['title']
-//        group.innerHTML = sources.data[j]['group_name']
-//        bundle_id.innerHTML = sources.data[j]['bundle_ID']
-//        compatibility.innerHTML = consensuses.data[i]['sources_map'][j]
-//    }
+    var table = document.createElement('table');
+    table.className = "sortable";
+    var row = table.insertRow(0);
+    var head_id = row.insertCell(0);
+    var head_name = row.insertCell(1);
+    var head_title = row.insertCell(2);
+    var head_group = row.insertCell(3);
+    var head_bundle_id = row.insertCell(4);
+    var head_compatibility = row.insertCell(5);
+    head_id.innerHTML = "ID"
+    head_name.innerHTML = "Name"
+    head_title.innerHTML = "Title"
+    head_group.innerHTML = "Group"
+    head_bundle_id.innerHTML = "Bundle ID"
+    head_compatibility.innerHTML = "Compatibility"
 
-//    compatibility_chart = get_density_graph(i);
-//
-//    consensus_info_div.appendChild(table)
-//    consensus_info_div.appendChild(compatibility_chart)
-//
-//    divId.appendChild(consensus_info_div);
+    for(var j=0;j<consensuses.data[i]['sources_map'].length;j++){
+        var row = table.insertRow(j+1);
+        var id = row.insertCell(0);
+        var name = row.insertCell(1);
+        var title = row.insertCell(2);
+        var group =row.insertCell(3)
+        var bundle_id = row.insertCell(4);
+        var compatibility = row.insertCell(5);
+        id.innerHTML = j
+        name.innerHTML = sources.data[j]['name']
+        title.innerHTML = sources.data[j]['title']
+        group.innerHTML = sources.data[j]['group_name']
+        bundle_id.innerHTML = sources.data[j]['bundle_ID']
+        compatibility.innerHTML = consensuses.data[i]['sources_map'][j]
+    }
+
+    compatibility_chart = get_density_graph(i);
+
+    consensus_info_div.appendChild(table)
+    consensus_info_div.appendChild(compatibility_chart)
+
+    divId.appendChild(consensus_info_div);
   }
 });
