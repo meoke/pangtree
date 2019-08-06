1425ae0acafceebe2f565b60205bdb10b4c1bc48
meoke <pedziadkiewicz@gmail.com>
2019-08-06 23:31:13 +0200

    Modify memory and time usage visualisation

--- scripts/make_charts.py
+++ scripts/make_charts.py
@@ -4,44 +4,88 @@ import csv
 from typing import List, Dict
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+import numpy as np
 
 data = Path(sys.argv[1]).resolve()
 dataset_name = sys.argv[2]
-time_plot_path = Path(sys.argv[3]).resolve()
-memory_plot_path = Path(sys.argv[4]).resolve()
+plot_path = Path(sys.argv[3]).resolve()
+data_output_path = Path(sys.argv[4]).resolve()
 
-p_to_memory_values: Dict[float, List[int]] = {}
-p_to_time_values: Dict[float, List[int]] = {}
+def version1(memory_plot_path, time_plot_path):
+    p_to_memory_values: Dict[float, List[int]] = {}
+    p_to_time_values: Dict[float, List[int]] = {}
 
-with open(data) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    next(csv_reader, None)
-    line_count = 0
-    for row in csv_reader:
-        if row[2] in p_to_memory_values:
-            p_to_memory_values[row[2]].append(int(row[4]))
-            p_to_time_values[row[2]].append(float(row[3]))
-        else:
-            p_to_memory_values[row[2]] = [int(row[4])]
-            p_to_time_values[row[2]] = [float(row[3])]
+    with open(data) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader, None)
+        line_count = 0
+        for row in csv_reader:
+            p = row[2]
+            if row[2] in p_to_memory_values:
+                p_to_memory_values[p].append(int(row[4]))
+                p_to_time_values[p].append(float(row[3]))
+            else:
+                p_to_memory_values[p] = [int(row[4])]
+                p_to_time_values[p] = [float(row[3])]
 
-fig, ax = plt.subplots()
-for p, memory_values in p_to_memory_values.items():
-    ax.plot(list(range(len(memory_values))), memory_values, label=p)
+    fig, ax = plt.subplots()
+    for p, memory_values in p_to_memory_values.items():
+        ax.plot(list(range(len(memory_values))), memory_values, label=p)
 
-ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-ax.set(xlabel='Iteration', ylabel='Memory [kB]', title=f"Memory usage - dataset {dataset_name}")
-ax.legend(loc=1)
-ax.grid()
-plt.xticks(list(range(len(p_to_memory_values["1"]))))
-fig.savefig(memory_plot_path)
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.set(xlabel='Iteration', ylabel='Memory [kB]', title=f"Memory usage - dataset {dataset_name}")
+    ax.legend(loc=1)
+    ax.grid()
+    plt.xticks(list(range(len(p_to_memory_values["1"]))))
+    fig.savefig(memory_plot_path)
 
-fig, ax = plt.subplots()
-for p, time_values in p_to_time_values.items():
-    ax.plot(list(range(len(time_values))), time_values, label=p)
+    fig, ax = plt.subplots()
+    for p, time_values in p_to_time_values.items():
+        ax.plot(list(range(len(time_values))), time_values, label=p)
 
-ax.set(xlabel='Iteration', ylabel='Time [s]', title=f"Time usage - dataset {dataset_name}")
-plt.xticks(list(range(len(p_to_time_values["1"]))))
-ax.legend(loc=1)
-ax.grid()
-fig.savefig(time_plot_path)
+    ax.set(xlabel='Iteration', ylabel='Time [s]', title=f"Time usage - dataset {dataset_name}")
+    plt.xticks(list(range(len(p_to_time_values["1"]))))
+    ax.legend(loc=1)
+    ax.grid()
+    fig.savefig(time_plot_path)
+
+
+def version2():
+    p_to_memory_values: Dict[float, List[int]] = {}
+    p_to_time_values: Dict[float, List[int]] = {}
+
+    with open(data) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader, None)
+        for row in csv_reader:
+            p = row[2]
+            if row[2] in p_to_memory_values:
+                p_to_memory_values[p].append(int(row[4]))
+                p_to_time_values[p].append(float(row[3]))
+            else:
+                p_to_memory_values[p] = [int(row[4])]
+                p_to_time_values[p] = [float(row[3])]
+
+    fig, ax = plt.subplots()
+
+    p_m_t = []
+    for p in p_to_memory_values.keys():
+        p_m_t.append((p, np.mean(p_to_memory_values[p]), np.mean(p_to_time_values[p])))
+    # X Axis - Memory
+    # Y Axis - Time
+    ax.scatter([d[1] for d in p_m_t], [d[2] for d in p_m_t])
+    for d in p_m_t:
+        ax.annotate(d[0], (d[1], d[2]))
+
+    ax.set(xlabel='Memory [kB]', ylabel='Time [s]', title=f"Resources usage")
+    fig.savefig(plot_path)
+
+    with open(data_output_path, mode='w') as csv_file:
+        resources_usage_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        resources_usage_writer.writerow(["P", "Memory", "Time"])
+        for d in p_m_t:
+            resources_usage_writer.writerow([d[0], d[1], d[2]])
+
+
+
+version2()
--- scripts/simulation_analysis.sh
+++ scripts/simulation_analysis.sh
@@ -1,7 +1,8 @@
 cd ..
 THEDATE=`date +%d%m_%H%M%S`
 #dataset_name="Ebola"
-dataset_name="Simulated3"
+#dataset_name="Simulated3"
+dataset_name="Simulated1"
 OUTPUTDIR="data/"$dataset_name"/output/"analysis_$THEDATE
 mkdir $OUTPUTDIR
 STATSFILEPATH=$OUTPUTDIR"/stats.csv"
@@ -10,19 +11,19 @@ echo "id,sample,p,runtime,memorypick" >> $STATSFILEPATH
 
 source venv/bin/activate
 i=0
-for sample in {0..3}; do
+for sample in {0..1}; do
   values=(0.25 0.5 1 2 4)
   labels=("025" "05" "1" "2" "4")
   for p in {0..4}; do
-    p_value=${values[$p]}
-    i=$((i+1))
-    ITERDIR=$OUTPUTDIR"/"$sample"_"${labels[$p]}"_output"
-    mkdir $ITERDIR
-    v=$( { /usr/bin/time -f "%e,%M" python3 -m pangtreebuild --output_dir $ITERDIR --multialignment "data/"$dataset_name"/input/yeast"$sample"_nocycles.maf" --consensus tree --p $p_value --stop 0.9999 --q; } 2>&1 )
-    echo $i","$sample","$p_value","$v >> $STATSFILEPATH
+      p_value=${values[$p]}
+      i=$((i+1))
+      ITERDIR=$OUTPUTDIR"/"$sample"_"${labels[$p]}"_"$t"_output"
+      mkdir $ITERDIR
+      v=$( { /usr/bin/time -f "%e,%M" python3 -m pangtreebuild --output_dir $ITERDIR --multialignment "data/"$dataset_name"/input/multialignment"$sample".maf" --consensus tree --p $p_value --stop 0.9999 --q; } 2>&1 )
+      echo $i","$sample","$p_value","$v >> $STATSFILEPATH
     done
 done
 
-python3 scripts/make_charts.py $STATSFILEPATH "$dataset_name" $OUTPUTDIR"/time_chart.png" $OUTPUTDIR"/memory_chart.png"
+python3 scripts/make_charts.py $STATSFILEPATH "$dataset_name" $OUTPUTDIR"/chart.png" $OUTPUTDIR"/data.csv"
 
 deactivate
