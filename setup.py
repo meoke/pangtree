5ab22556265adc92ade1c4324eb96b0f27289035
Paulina Dziadkiewicz <pedziadkiewicz@gmail.com>
2019-12-30 12:58:03 +0100

    Update readme and package version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='pangtreebuild',
-    version='1.0.7',
+    version='1.0.1',
     author="Paulina Dziadkiewicz",
     author_email="pedziadkiewicz@gmail.com",
     description="Multiple sequence alignment analysis with Affinity Tree generation",
